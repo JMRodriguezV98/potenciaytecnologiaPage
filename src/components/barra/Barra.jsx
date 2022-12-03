63a7c10afe5c7eaaabf8c3102ffb274aa129b47b
INGENIERO3 <INGENIERO3@pyt.local>
2022-12-03 14:06:19 -0500

    animacion scrolling

--- src/components/barra/Barra.jsx
+++ src/components/barra/Barra.jsx
@@ -6,32 +6,44 @@ const Barra = () => {
 
   const location = useLocation();
   const locationPath = location.pathname;
+  const barra = document.getElementById('statu');
+  const page = document.getElementById('page');
   let transformLocation = '0';
 
   switch ( locationPath ) {
     case '/':
       transformLocation = '01';
+      barra.style.height = '46px'
+      page.style.top = '135px';
     break;
 
     case '/nosotros':
       transformLocation = '02';
+      barra.style.height = '86px';
+      page.style.top = '175px';
     break;
 
     case '/productos':
       transformLocation = '03';
+      barra.style.height = '126px';
+      page.style.top = '215px';
     break;
 
     case '/catalogo':
       transformLocation = '04';
+      barra.style.height = '186px';
+      page.style.top = '275px';
     break;
 
     case '/contacto':
       transformLocation = '05';
+      barra.style.height = '400px';
+      page.style.top = '490px';
     break;
   
     default:
 
-      break;
+    break;
   }
 
   return (
